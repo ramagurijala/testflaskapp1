3189aa84e287b976d1cded39e09225ad6d44ab4c
ramagurijala <grkrishna_mca@yahoo.com>
2019-03-16 14:11:54 +0000

    22

--- flask-hello-world-master/hello.py
+++ flask-hello-world-master/hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'First  on Github Jenkins Python Flask World!'
+    return 'First success on Github Jenkins Python Flask World!'
 
 if __name__ == '__main__':
     app.run()
