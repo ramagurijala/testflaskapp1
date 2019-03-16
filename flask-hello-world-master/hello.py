2e1a1f5208ae30957fc24a78a4e0d1df9826d88e
ramagurijala <grkrishna_mca@yahoo.com>
2019-03-16 11:57:36 +0000

    2ndgmail

--- flask-hello-world-master/hello.py
+++ flask-hello-world-master/hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello 12345 test  abc Flask World!'
+    return 'Hello XYZ test  abc Flask World!'
 
 if __name__ == '__main__':
     app.run()
